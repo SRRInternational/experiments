83a2defde9cb9fc538f43b877fb099ffd9e470d8
Akshay Moolya <akshay.moolya@srrintl.com>
2024-02-27 18:37:12 +0530

    feat:added login page

--- usermanagement/login.php
+++ usermanagement/login.php
@@ -20,33 +20,18 @@ if (isset($_SESSION['username'])) {
 
 </head>
 
-<body class="bg-gray-100 flex justify-center items-center h-screen">
+<body class="bg-gray-100 flex justify-center items-center h-screen flex-col">
 
     <div class="bg-white flex gap-4 p-8 justify-between w-[1100px] rounded-3xl shadow-md">
         <div class="space-y-5 flex flex-col w-full">
             <div class="w-16 h-16">
                 <image src="./images/google.png" alt="google" />
             </div>
-            <P class=" text-3xl font-normal">Create Your Account</P>
+            <P class=" text-3xl font-normal">Login to Your Account</P>
             <p class="text-2xl">Enter You Details</p>
 
         </div>
-        <form class="w-full" method="post" enctype="multipart/form-data">
-            <div class="mb-4">
-                <label class="mb-2.5 block font-medium text-black">Name</label>
-                <div class="relative">
-                    <input name="username" type="text" placeholder="Enter your full name" class="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-blue-600 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-blue-600" />
-
-                    <span class="absolute right-4 top-4">
-                        <svg class="fill-current" width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g opacity="0.5">
-                                <path d="M11.0008 9.52185C13.5445 9.52185 15.607 7.5281 15.607 5.0531C15.607 2.5781 13.5445 0.584351 11.0008 0.584351C8.45703 0.584351 6.39453 2.5781 6.39453 5.0531C6.39453 7.5281 8.45703 9.52185 11.0008 9.52185ZM11.0008 2.1656C12.6852 2.1656 14.0602 3.47185 14.0602 5.08748C14.0602 6.7031 12.6852 8.00935 11.0008 8.00935C9.31641 8.00935 7.94141 6.7031 7.94141 5.08748C7.94141 3.47185 9.31641 2.1656 11.0008 2.1656Z" fill="" />
-                                <path d="M13.2352 11.0687H8.76641C5.08828 11.0687 2.09766 14.0937 2.09766 17.7719V20.625C2.09766 21.0375 2.44141 21.4156 2.88828 21.4156C3.33516 21.4156 3.67891 21.0719 3.67891 20.625V17.7719C3.67891 14.9531 5.98203 12.6156 8.83516 12.6156H13.2695C16.0883 12.6156 18.4258 14.9187 18.4258 17.7719V20.625C18.4258 21.0375 18.7695 21.4156 19.2164 21.4156C19.6633 21.4156 20.007 21.0719 20.007 20.625V17.7719C19.9039 14.0937 16.9133 11.0687 13.2352 11.0687Z" fill="" />
-                            </g>
-                        </svg>
-                    </span>
-                </div>
-            </div>
+        <form class="w-full" method="post" enctype="multipart/form-data" class="flex flex-col justify-center">
 
             <div class="mb-4">
                 <label class="mb-2.5 block font-medium text-black ">Email</label>
@@ -78,18 +63,12 @@ if (isset($_SESSION['username'])) {
                     </span>
                 </div>
             </div>
-
-            <div class="mb-3">
-                <label class="mb-3 block text-sm font-medium text-black ">
-                    Attach file
-                </label>
-                <input type="file" name="image" class="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent font-normal outline-none transition file:mr-5 file:border-none file:cursor-pointer file:border-0 file:border-r  file:border-stroke file:bg-whiter file:px-5 file:py-3 file:hover:bg-blue-600 file:hover:bg-opacity-10 focus:border-blue-600 active:border-primary disabled:cursor-default disabled:bg-whiter " />
-            </div>
             <div class=" w-full mb-4 flex items-end justify-end ">
-                <button type="submit" class="self-end  bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">Create account</button>
+                <button type="submit" class="self-end  bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">Log in</button>
             </div>
         </form>
     </div>
+
 
 </body>
 
