9a2b05e04016b556ae8cb9dafa590aa779b9cc59
Akshay Moolya <akshay.moolya@srrintl.com>
2024-02-28 14:56:10 +0530

    feat: fixed styling for login and register page

--- usermanagement/index.php
+++ usermanagement/index.php
@@ -20,75 +20,67 @@ if (isset($_SESSION['username'])) {
 
 </head>
 
-<body class="bg-gray-100 flex justify-center items-center h-screen">
+<body class="bg-blue-100/25 flex flex-col justify-center items-center h-screen">
+
+
 
     <div class="bg-white flex gap-4 p-8 justify-between w-[1100px] rounded-3xl shadow-md">
         <div class="space-y-5 flex flex-col w-full">
-            <div class="w-16 h-16">
-                <image src="./images/google.png" alt="google" />
+            <div class="w-16 h-16 ">
+                <image src="./images/logo.png" class="object-cover w-full h-full" alt="google" />
             </div>
             <P class=" text-3xl font-normal">Create Your Account</P>
             <p class="text-2xl">Enter Your Details</p>
 
         </div>
         <form class="w-full" method="post" enctype="multipart/form-data">
-            <div class="mb-4">
-                <label class="mb-2.5 block font-medium text-black">Name</label>
-                <div class="relative">
-                    <input name="username" type="text" placeholder="Enter your full name" class="w-full rounded-lg border border-black bg-transparent py-4 pl-6 pr-10 outline-none focus:border-blue-600 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-blue-600" />
-
-                    <span class="absolute right-4 top-4">
-                        <svg class="fill-current" width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g opacity="0.5">
-                                <path d="M11.0008 9.52185C13.5445 9.52185 15.607 7.5281 15.607 5.0531C15.607 2.5781 13.5445 0.584351 11.0008 0.584351C8.45703 0.584351 6.39453 2.5781 6.39453 5.0531C6.39453 7.5281 8.45703 9.52185 11.0008 9.52185ZM11.0008 2.1656C12.6852 2.1656 14.0602 3.47185 14.0602 5.08748C14.0602 6.7031 12.6852 8.00935 11.0008 8.00935C9.31641 8.00935 7.94141 6.7031 7.94141 5.08748C7.94141 3.47185 9.31641 2.1656 11.0008 2.1656Z" fill="" />
-                                <path d="M13.2352 11.0687H8.76641C5.08828 11.0687 2.09766 14.0937 2.09766 17.7719V20.625C2.09766 21.0375 2.44141 21.4156 2.88828 21.4156C3.33516 21.4156 3.67891 21.0719 3.67891 20.625V17.7719C3.67891 14.9531 5.98203 12.6156 8.83516 12.6156H13.2695C16.0883 12.6156 18.4258 14.9187 18.4258 17.7719V20.625C18.4258 21.0375 18.7695 21.4156 19.2164 21.4156C19.6633 21.4156 20.007 21.0719 20.007 20.625V17.7719C19.9039 14.0937 16.9133 11.0687 13.2352 11.0687Z" fill="" />
-                            </g>
-                        </svg>
-                    </span>
+            <div class="my-4">
+                <div class="relative w-full h-10 ">
+                    <input name="username" type="password" class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-gray-800 focus:border-t-transparent text-m px-3 py-2.5 rounded-[7px] border-gray-800 focus:border-blue-600" placeholder=" " /><label class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[14px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-blue-600 before:border-blue-gray-200 peer-focus:before:!border-blue-600 after:border-blue-600 peer-focus:after:!border-blue-600">Username
+                    </label>
                 </div>
             </div>
 
-            <div class="mb-4">
-                <label class="mb-2.5 block font-medium text-black ">Email</label>
-                <div class="relative">
-                    <input type="email" name="email" placeholder="Enter your email" class="w-full rounded-lg border border-black bg-transparent py-4 pl-6 pr-10 outline-none focus:border-blue-600 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-blue-600" />
-
-                    <span class="absolute right-4 top-4">
-                        <svg class="fill-current" width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g opacity="0.5">
-                                <path d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z" fill="" />
-                            </g>
-                        </svg>
-                    </span>
+            <div class="my-4">
+                <div class="relative w-full h-10 ">
+                    <input name="email" type="password" class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-gray-800 focus:border-t-transparent text-m px-3 py-2.5 rounded-[7px] border-gray-800 focus:border-blue-600" placeholder=" " /><label class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[14px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-blue-600 before:border-blue-gray-200 peer-focus:before:!border-blue-600 after:border-blue-600 peer-focus:after:!border-blue-600">Email
+                    </label>
                 </div>
             </div>
 
-            <div class="mb-4">
-                <label class="mb-2.5 block font-medium text-black ">Password</label>
-                <div class="relative">
-                    <input type="password" name="password" placeholder="Enter your password" class="w-full rounded-lg border border-black bg-transparent py-4 pl-6 pr-10 outline-none focus:border-blue-600 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-blue-600" />
-
-                    <span class="absolute right-4 top-4">
-                        <svg class="fill-current" width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g opacity="0.5">
-                                <path d="M16.1547 6.80626V5.91251C16.1547 3.16251 14.0922 0.825009 11.4797 0.618759C10.0359 0.481259 8.59219 0.996884 7.52656 1.95938C6.46094 2.92188 5.84219 4.29688 5.84219 5.70626V6.80626C3.84844 7.18438 2.33594 8.93751 2.33594 11.0688V17.2906C2.33594 19.5594 4.19219 21.3813 6.42656 21.3813H15.5016C17.7703 21.3813 19.6266 19.525 19.6266 17.2563V11C19.6609 8.93751 18.1484 7.21876 16.1547 6.80626ZM8.55781 3.09376C9.31406 2.40626 10.3109 2.06251 11.3422 2.16563C13.1641 2.33751 14.6078 3.98751 14.6078 5.91251V6.70313H7.38906V5.67188C7.38906 4.70938 7.80156 3.78126 8.55781 3.09376ZM18.1141 17.2906C18.1141 18.7 16.9453 19.8688 15.5359 19.8688H6.46094C5.05156 19.8688 3.91719 18.7344 3.91719 17.325V11.0688C3.91719 9.52189 5.15469 8.28438 6.70156 8.28438H15.2953C16.8422 8.28438 18.1141 9.52188 18.1141 11V17.2906Z" fill="" />
-                                <path d="M10.9977 11.8594C10.5852 11.8594 10.207 12.2031 10.207 12.65V16.2594C10.207 16.6719 10.5508 17.05 10.9977 17.05C11.4102 17.05 11.7883 16.7063 11.7883 16.2594V12.6156C11.7883 12.2031 11.4102 11.8594 10.9977 11.8594Z" fill="" />
-                            </g>
-                        </svg>
-                    </span>
+            <div class="my-4">
+                <div class="relative w-full h-10 ">
+                    <input name="password" type="password" class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-gray-800 focus:border-t-transparent text-m px-3 py-2.5 rounded-[7px] border-gray-800 focus:border-blue-600" placeholder=" " /><label class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[14px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-blue-600 before:border-blue-gray-200 peer-focus:before:!border-blue-600 after:border-blue-600 peer-focus:after:!border-blue-600">Password
+                    </label>
                 </div>
             </div>
 
             <div class="mb-3">
-                <label class="mb-3 block text-sm font-medium text-black ">
+                <label class="mb-2 block text-sm font-medium text-black ">
                     Attach file
                 </label>
-                <input type="file" name="image" class="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent font-normal outline-none transition file:mr-5 file:border-none file:cursor-pointer file:border-0 file:border-r  file:border-stroke file:bg-whiter file:px-5 file:py-3 file:hover:bg-blue-600 file:hover:bg-opacity-10 focus:border-blue-600 active:border-primary disabled:cursor-default disabled:bg-whiter " />
+                <input type="file" name="image" class="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent font-normal outline-none transition file:mr-5 file:border-none file:cursor-pointer file:border-0 file:border-r  file:border-stroke file:bg-whiter file:px-5 file:py-2 file:hover:bg-blue-600 file:hover:bg-opacity-10 focus:border-blue-600 active:border-primary disabled:cursor-default disabled:bg-whiter " />
             </div>
-            <div class=" w-full mb-4 flex items-end justify-end ">
-                <button type="submit" class="self-end  bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">Create account</button>
+            <div class=" w-full mt-4 flex items-end justify-end ">
+                <button type="submit" class="self-end  bg-blue-700 text-white py-2 px-4 rounded-3xl hover:bg-blue-600">Log in</button>
             </div>
         </form>
+    </div>
+    <div class=" flex justify-between w-[1100px] mt-2">
+        <form class="max-w-[200px] w-full">
+            <select id="countries" class="bg-transparent hover:bg-gray-300/50 border-2 border-transparent text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full py-1.5 px-2.5">
+                <option selected>English (United Kingdon)</option>
+                <option value="US">United States</option>
+                <option value="CA">Canada</option>
+                <option value="FR">France</option>
+                <option value="DE">Germany</option>
+            </select>
+        </form>
+        <div class=" flex gap-4">
+            <button class="text-xs hover:bg-gray-300/50 py-1 px-4 rounded-xl">Help</button>
+            <button class="text-xs hover:bg-gray-300/50 py-1 px-4 rounded-xl">Privacy</button>
+            <button class="text-xs hover:bg-gray-300/50 py-1 px-4 rounded-xl">Term </button>
+        </div>
     </div>
 
 </body>
--- usermanagement/login.php
+++ usermanagement/login.php
@@ -19,7 +19,7 @@ if (isset($_SESSION['username'])) {
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="bg-gray-100 flex justify-center items-center h-screen flex-col">
+<body class="bg-blue-100/25 flex justify-center items-center h-screen flex-col">
 
     <div class="bg-white flex gap-4 p-8 justify-between w-[1100px] rounded-3xl shadow-md">
         <div class="space-y-5 flex flex-col w-full">
@@ -45,10 +45,26 @@ if (isset($_SESSION['username'])) {
                 </div>
             </div>
 
-            <div class=" w-full mb-4 flex items-end justify-end ">
-                <button type="submit" class="self-end  bg-blue-700 text-white py-2 px-4 rounded-md hover:bg-blue-600">Log in</button>
+            <div class=" w-full mt-4 flex items-end justify-end ">
+                <button type="submit" class="self-end  bg-blue-700 text-white py-2 px-4 rounded-3xl hover:bg-blue-600">Log in</button>
             </div>
         </form>
+    </div>
+    <div class=" flex justify-between w-[1100px] mt-2">
+        <form class="max-w-[200px] w-full">
+            <select id="countries" class="bg-transparent hover:bg-gray-300/50 border-2 border-transparent text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full py-1.5 px-2.5">
+                <option selected>English (United Kingdon)</option>
+                <option value="US">United States</option>
+                <option value="CA">Canada</option>
+                <option value="FR">France</option>
+                <option value="DE">Germany</option>
+            </select>
+        </form>
+        <div class=" flex gap-4">
+            <button class="text-xs hover:bg-gray-300/50 py-1 px-4 rounded-xl">Help</button>
+            <button class="text-xs hover:bg-gray-300/50 py-1 px-4 rounded-xl">Privacy</button>
+            <button class="text-xs hover:bg-gray-300/50 py-1 px-4 rounded-xl">Term </button>
+        </div>
     </div>
 
 
@@ -57,66 +73,31 @@ if (isset($_SESSION['username'])) {
 <?php
 // Check if the form is submitted, validate the data, and add the user in the database
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST['email'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    if (isset($_POST['login'])) {
+        // Handle login
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-    // Validate email
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Invalid email format";
-        exit();
-    }
+        // SQL query to retrieve user information based on the username
+        $query = "SELECT * FROM user WHERE username='$username'";
+        $result = mysqli_query($conn, $query);
 
-    // Encrypt the password
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-    // Process image upload
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["image"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-    // Check if image file is a actual image or fake image
-    $check = getimagesize($_FILES["image"]["tmp_name"]);
-    if ($check !== false) {
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-    // Check file size
-    if ($_FILES["image"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-    // Allow certain file formats
-    if (
-        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif"
-    ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-        // if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-            echo "The file " . htmlspecialchars(basename($_FILES["image"]["name"])) . " has been uploaded.";
-            // SQL query to insert a new user with default CreatedAt time
-            $query = "INSERT INTO user (email, username, password, image) VALUES ('$email', '$username', '$hashed_password', '$target_file')";
-
-            if (mysqli_query($conn, $query)) {
-                // User registered successfully, start the session and redirect to admin panel
+        if ($result && mysqli_num_rows($result) > 0) {
+            $user = mysqli_fetch_assoc($result);
+            // Verify password
+            if (password_verify($password, $user['password'])) {
+                // Password is correct, start session and redirect to admin panel
+                session_start();
                 $_SESSION['username'] = $username;
                 header("Location: AdminPanel.php");
                 exit();
             } else {
-                // Error occurred during registration
-                echo "Error: " . $query . "<br>" . mysqli_error($conn);
+                // Incorrect password
+                echo "Incorrect username or password.";
             }
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            // User not found
+            echo "User not found.";
         }
     }
 }
