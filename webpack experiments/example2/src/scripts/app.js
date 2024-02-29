e909b281e20085a63710c9d825e962788a8384b9
Akshay Moolya <akshay.moolya@srrintl.com>
2024-02-29 14:20:25 +0530

    feat:craeted example 2

---
+++ webpack experiments/example2/src/scripts/app.js
@@ -0,0 +1,4 @@
+console.log('Hello from app.js!');
+import { greet } from './utils/helper.js';
+
+greet();
---
+++ webpack experiments/example2/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+};
