d0bcbc6c45f7d43608387154ddadd80b2dde7b6a
Akshay Moolya <akshay.moolya@srrintl.com>
2024-02-29 15:26:47 +0530

    feat: created example1

---
+++ mustache experiments/example1/render.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const mustache = require("mustache");
+
+// Read the template file
+const template = fs.readFileSync("index.html", "utf8");
+
+// Read the data file
+const data = fs.readFileSync("data.json", "utf8");
+
+// Parse JSON data
+const jsonData = JSON.parse(data);
+
+// Render the template with the data
+const output = mustache.render(template, jsonData);
+
+// Output the rendered HTML
+console.log(output);
