ec6cc85331ae5583ccd99939963bfb7b01cc38e0
Harsh Jain <harsh.jain@srrintl.com>
2024-07-05 17:02:57 +0530

    Experimenting with vanilla and lexical. created examples for ts and js

---
+++ editor/Experiment-Lexical/lexical-vite-js/editor/index.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import "./css/styles.css";
+
+import { registerDragonSupport } from "@lexical/dragon";
+import { createEmptyHistoryState, registerHistory } from "@lexical/history";
+import { HeadingNode, QuoteNode, registerRichText } from "@lexical/rich-text";
+import { mergeRegister } from "@lexical/utils";
+import { createEditor } from "lexical";
+
+function editor(el) {
+  const initialConfig = {
+    namespace: "Vanilla JS Demo",
+    nodes: [HeadingNode, QuoteNode],
+    onError: console.log,
+    theme: {
+      quote: "PlaygroundEditorTheme__quote",
+    },
+  };
+  const editor = createEditor(initialConfig);
+
+  mergeRegister(
+    registerRichText(editor),
+    registerDragonSupport(editor),
+    registerHistory(editor, createEmptyHistoryState(), 300)
+  );
+  editor.setRootElement(el);
+}
+
+export default editor;
