14208785727d48798959dbbcb02fa67c866fd264
Akshay Moolya <akshay.moolya@srrintl.com>
2024-07-18 17:58:02 +0530

    feat: Configure Webpack to convert @lexical to AMD for RequireJS experiment
    - Created a Webpack configuration to transpile the entire `node_modules/@lexical` directory into AMD format.

--- lexical-experiment/example-7/js/app/views/EditorViews.js
+++ lexical-experiment/example-7/js/app/views/EditorViews.js
@@ -5,11 +5,28 @@ define(["jquery", "backbone", "lexical"], function ($, Backbone, lexical) {
       "click #bold-button": "makeBold",
       "click #italic-button": "makeItalic",
       "click #underline-button": "makeUnderline",
+      "click #strikethrough-button": "makeStrikethrough",
+      "click #left-align-button": "leftAlign",
+      "click #center-align-button": "centerAlign",
+      "click #right-align-button": "rightAlign",
+      "click #justify-align-button": "justifyAlign",
+      "click #code-button": "makeCode",
+      "click #quote-button": "makeQuote",
+      "click #bullet-list-button": "makeBulletList",
+      "click #numbered-list-button": "makeNumberedList",
+      "click #link-button": "makeLink",
+      "click #h1-button": "makeH1",
+      "click #h2-button": "makeH2",
+      "click #highlight-button": "makeHighlight",
+      "click #subscript-button": "makeSubscript",
+      "click #superscript-button": "makeSuperscript",
+      "click #clear-formatting-button": "clearFormatting",
     },
     initialize: function () {
       this.editor = lexical.createEditor();
-      this.$editor = this.$("#editor");
+      this.$editor = this.$("#editor-container");
       this.listenTo(this.model, "change:content", this.render);
+      this.initSelectionChangeListener();
     },
     render: function () {
       this.$editor.html(this.model.get("content"));
@@ -27,8 +44,110 @@ define(["jquery", "backbone", "lexical"], function ($, Backbone, lexical) {
       document.execCommand("underline");
       this.updateContent();
     },
+    makeStrikethrough: function () {
+      document.execCommand("strikethrough");
+      this.updateContent();
+    },
+    leftAlign: function () {
+      document.execCommand("justifyLeft");
+      this.updateContent();
+    },
+    centerAlign: function () {
+      document.execCommand("justifyCenter");
+      this.updateContent();
+    },
+    rightAlign: function () {
+      document.execCommand("justifyRight");
+      this.updateContent();
+    },
+    justifyAlign: function () {
+      document.execCommand("justifyFull");
+      this.updateContent();
+    },
+    makeCode: function () {
+      document.execCommand("formatBlock", false, "<pre>");
+      this.updateContent();
+    },
+    makeQuote: function () {
+      document.execCommand("formatBlock", false, "<blockquote>");
+      this.updateContent();
+    },
+    makeBulletList: function () {
+      document.execCommand("insertUnorderedList");
+      this.updateContent();
+    },
+    makeNumberedList: function () {
+      document.execCommand("insertOrderedList");
+      this.updateContent();
+    },
+    makeLink: function () {
+      const url = prompt("Enter the URL");
+      if (url) {
+        document.execCommand("createLink", false, url);
+      }
+      this.updateContent();
+    },
+    makeH1: function () {
+      document.execCommand("formatBlock", false, "<h1>");
+      this.updateContent();
+    },
+    makeH2: function () {
+      document.execCommand("formatBlock", false, "<h2>");
+      this.updateContent();
+    },
+    makeHighlight: function () {
+      document.execCommand("hiliteColor", false, "yellow");
+      this.updateContent();
+    },
+    makeSubscript: function () {
+      document.execCommand("subscript");
+      this.updateContent();
+    },
+    makeSuperscript: function () {
+      document.execCommand("superscript");
+      this.updateContent();
+    },
+    clearFormatting: function () {
+      document.execCommand("removeFormat");
+      this.updateContent();
+    },
     updateContent: function () {
       this.model.set("content", this.$editor.html());
+      this.updateToolbarButtons();
+    },
+    updateToolbarButtons: function () {
+      this.toggleButtonActiveState("#bold-button", "bold");
+      this.toggleButtonActiveState("#italic-button", "italic");
+      this.toggleButtonActiveState("#underline-button", "underline");
+      this.toggleButtonActiveState("#strikethrough-button", "strikethrough");
+      this.toggleButtonActiveState("#left-align-button", "justifyLeft");
+      this.toggleButtonActiveState("#center-align-button", "justifyCenter");
+      this.toggleButtonActiveState("#right-align-button", "justifyRight");
+      this.toggleButtonActiveState("#justify-align-button", "justifyFull");
+      this.toggleButtonActiveState("#code-button", "formatBlock", "<pre>");
+      this.toggleButtonActiveState(
+        "#quote-button",
+        "formatBlock",
+        "<blockquote>"
+      );
+      this.toggleButtonActiveState(
+        "#bullet-list-button",
+        "insertUnorderedList"
+      );
+      this.toggleButtonActiveState(
+        "#numbered-list-button",
+        "insertOrderedList"
+      );
+    },
+    toggleButtonActiveState: function (buttonSelector, command, value) {
+      const isActive = document.queryCommandState(command, value);
+      this.$(buttonSelector).toggleClass("active", isActive);
+    },
+    initSelectionChangeListener: function () {
+      document.addEventListener(
+        "selectionchange",
+        this.updateToolbarButtons.bind(this)
+      );
     },
   });
 });
--- lexical-experiment/example-7/webpack.config.js
+++ lexical-experiment/example-7/webpack.config.js
@@ -1,24 +1,52 @@
 const path = require("path");
+const fs = require("fs");
+
+// Helper function to get all .dev.mjs files under @lexical
+function getLexicalDevEntries() {
+  const lexicalDir = path.resolve(__dirname, "node_modules", "@lexical");
+  const entries = {};
+
+  function findDevFiles(dir) {
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+      const fullPath = path.join(dir, file);
+      if (fs.lstatSync(fullPath).isDirectory()) {
+        findDevFiles(fullPath);
+      } else if (file.endsWith(".dev.mjs")) {
+        const relativePath = path.relative(lexicalDir, fullPath);
+        entries[relativePath.replace(/\\/g, "/").replace(/\.dev\.mjs$/, "")] =
+          fullPath;
+      }
+    });
+  }
+
+  findDevFiles(lexicalDir);
+  return entries;
+}
 
 module.exports = {
-  entry: "/node_modules/lexical/lexical.dev.mjs",
+  entry: getLexicalDevEntries(),
   output: {
     path: path.resolve(__dirname, "js", "lib"),
-    filename: "lexical.amd.js",
+    filename: "[name].js",
     libraryTarget: "amd",
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
+        test: /\.mjs$/,
+        include: /node_modules\/@lexical/,
         use: {
           loader: "babel-loader",
           options: {
+            presets: ["@babel/preset-env"],
             plugins: ["@babel/plugin-transform-modules-amd"],
           },
         },
       },
     ],
+  },
+  resolve: {
+    extensions: [".js", ".mjs"],
   },
 };
